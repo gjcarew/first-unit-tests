041454e64689cd47b1817a43edef5ca100e6b0d3
Gavin Carew <gjcarew@gmail.com>
2023-05-16 14:28:01 -0700

    Get tests to pass

--- functions.js
+++ functions.js
@@ -1,9 +1,25 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(n1, n2) {
+    return n1 + n2
+}
+
+function sayHello(name) {
+    return name === 'Will' ? 'No more testing Will!' : `Hi there ${name}!`
+}
+
+function buildCar(color, type) {
+    if (color === undefined && type === undefined) {
+        return {}
+    }
+    
+    return {
+        color: color,
+        type: type
+    }
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
